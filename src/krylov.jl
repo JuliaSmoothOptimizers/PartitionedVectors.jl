d611b72b927f13a13f09fb79a730ec19ccc6f74b
RAYNAUD Paul (raynaudp) <raynaudp@grenoble-inp.lan>
2022-10-05 19:48:52 -0300

    fix LinearOperator + finalize test

--- src/krylov.jl
+++ src/krylov.jl
@@ -29,6 +29,11 @@ function axpy!(s::Y, x::PartitionedVector{T}, y::PartitionedVector{T}, ::Val{fal
   return y
 end
 
+function axpy!(s::Y, x::PartitionedVector{T}, y::PartitionedVector{T}, ::Val{false}, ::Val{false}) where {T<:Number,Y<:Number}
+  y .+= s .* x
+  return y
+end
+
 function axpby!(s::Y1, x::PartitionedVector{T}, t::Y2, y::PartitionedVector{T}) where {T<:Number,Y1<:Number,Y2<:Number}
   axpby!(s, x, t, y, Val(x.simulate_vector), Val(y.simulate_vector))
 end
@@ -42,15 +47,26 @@ function axpby!(s::Y1, x::PartitionedVector{T}, t::Y2, y::PartitionedVector{T}, 
   return y
 end
 
-# function axpby!(s::Y1, x::PartitionedVector{T}, t::Y2, y::PartitionedVector{T}, ::Val{false}, ::Val{false}) where {T<:Number,Y1<:Number,Y2<:Number}
-#   println("test")
-#   y .= x .* s .+ y .* t
+# function axpby!(s::Y1, x::PartitionedVector{T}, t::Y2, y::PartitionedVector{T}, ::Val{false}, ::Val{true}) where {T<:Number,Y1<:Number,Y2<:Number}
+#   build!(x)
+#   build!(y)
+#   xvector = x.epv.v
+#   yvector = y.epv.v
+#   epv_from_v!(y.epv, s .* xvector .+ yvector .* t)
 #   return y
 # end
+
+function axpby!(s::Y1, x::PartitionedVector{T}, t::Y2, y::PartitionedVector{T}, ::Val{false}, ::Val{false}) where {T<:Number,Y1<:Number,Y2<:Number}
+  y .= x .* s .+ y .* t
+  return y
+end
 
 function axpby!(s::Y1, x::PartitionedVector{T}, t::Y2, y::PartitionedVector{T}, ::Val{true}, ::Val{true}) where {T<:Number,Y1<:Number,Y2<:Number}
   y .= x .* s .+ y .* t
 end
+
+
+# mul!(y, P, x)
 
 
 function CgSolver(pv::PartitionedVector{T}) where T  
