c36bd68a80ea8bd7dd61a69eb04140a12c5b4a52
RAYNAUD Paul (raynaudp) <raynaudp@grenoble-inp.lan>
2022-11-07 12:34:42 -0400

    remove unused code from src/broadcast.jl

--- src/broadcast.jl
+++ src/broadcast.jl
@@ -4,15 +4,8 @@ struct PartitionedVectorStyle <: Base.Broadcast.BroadcastStyle end
 
 Base.BroadcastStyle(::Type{PartitionedVector{T}}) where T = PartitionedVectorStyle()
 
-Base.BroadcastStyle(::PartitionedVectorStyle, ::PartitionedVectorStyle) = PartitionedVectorStyle()
 Base.BroadcastStyle(::PartitionedVectorStyle, ::Base.Broadcast.BroadcastStyle) = PartitionedVectorStyle()
 Base.BroadcastStyle(::Base.Broadcast.BroadcastStyle, ::PartitionedVectorStyle) = PartitionedVectorStyle()
-
-function Base.similar(bc::Base.Broadcast.Broadcasted{PartitionedVectorStyle}, ::Type{ElType}) where ElType
-  pv = find_pv(bc)
-  pvres = similar(pv)  
-  return pvres
-end
 
 function Base.copyto!(dest::PartitionedVector, bc::Base.Broadcast.Broadcasted{PartitionedVectorStyle})
   bcf = Base.Broadcast.flatten(bc)
@@ -29,9 +22,3 @@ _filter(i::Int, arg::Tuple{}) = ()
 _filter(i::Int, arg::PartitionedVector) = arg[i]
 _filter(i::Int, arg::Any) = arg
 _filter(i::Int, args::Tuple) = (_filter(i, args[1]), _filter(i, Base.tail(args))...)
-
-# Select a PartitionnedVector in a Broadcasted tape
-find_pv(bc::Base.Broadcast.Broadcasted) = find_pv(bc.args...)
-find_pv(x::PartitionedVector) = x
-find_pv(x::PartitionedVector, a::Any) = x
-find_pv(x::Any, a::Any) = find_pv(a)
