a48e5cff80d3e34291662ee4cadd584d39ea6b9d
RAYNAUD Paul (raynaudp) <raynaudp@grenoble-inp.lan>
2022-12-13 10:30:28 +0200

    dedicated dot(pv1,pv2) when pv1, pv2 have different usages

--- src/linearAlgebra.jl
+++ src/linearAlgebra.jl
@@ -8,7 +8,28 @@ function norm(pv::PartitionedVector, p::Real = 2; kwargs...)
   return _norm
 end
 
-function dot(pv1::PartitionedVector{T}, pv2::PartitionedVector{T}; kwargs...) where {T}
+dot(pv1::PartitionedVector{T}, pv2::PartitionedVector{T}; kwargs...) where {T} = dot(pv1, pv2, Val(pv1.simulate_vector), Val(pv2.simulate_vector); kwargs...)
+
+function dot(pv1::PartitionedVector{T}, pv2::PartitionedVector{T}, ::Val{true}, ::Val{false}; kwargs...) where {T}
+  N = pv1.epv.N
+  acc = 0
+  for i in 1:N
+    eev1 = pv1[i].vec
+    eev2 = pv2[i].vec
+    acc += dot(eev1, eev2)
+  end
+  return acc
+end
+
+dot(pv1::PartitionedVector{T}, pv2::PartitionedVector{T}, ::Val{false}, ::Val{true}; kwargs...) where {T} = dot(pv2, pv1)
+
+function dot(pv1::PartitionedVector{T}, pv2::PartitionedVector{T}, ::Val{true}, ::Val{true}; kwargs...) where {T}
+  build!(pv1; kwargs...)
+  build!(pv2; kwargs...)
+  dot(get_v(pv1.epv), get_v(pv2.epv))
+end
+
+function dot(pv1::PartitionedVector{T}, pv2::PartitionedVector{T}, ::Val{false}, ::Val{false}; kwargs...) where {T}
   build!(pv1; kwargs...)
   build!(pv2; kwargs...)
   dot(get_v(pv1.epv), get_v(pv2.epv))
